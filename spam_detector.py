f8390840a299722ebfdbf95e35dd29172ef7c882
Krishanu Konar <krishh_konar@yahoo.com>
2016-04-17 00:42:17 +0530

    SVM implemented for better accuracy.

--- spam_detector.py
+++ spam_detector.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 '''
-
+##
 ## A supervised Classifier that predicts whether a given piece of text(SMS) is
 ## SPAM or NOT SPAM(HAM)
-
+##
+## Classifier Used: Support Vector Machine
+## ----------------
+##
 ## Dataset used:
 ## --------------
 ## https://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
-
+##
 '''
 
 import matplotlib.pyplot as plt
@@ -20,12 +23,15 @@ import numpy as np
 import sys
 import string
 import unicodedata
+import cPickle
+import time
 
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
@@ -50,74 +56,140 @@ stop_words = stopwords.words("english")
 
 
 def main():
+	#loads training data
 	data = load_dataset()
-	clf_NB = MultinomialNB()
-	
+		
 	### splitting training and testing data
 	message_train, message_test, label_train, label_test = train_test_split(data['message'],data['label'],test_size=0.1)
 
+	try:
+		try:
+			## checks for exisiting classifier
+			spam_detector_SVM = cPickle.load(open("spam_detector_classifier.pkl","r"))
+			opt = int(raw_input('Classifier found. Continue(0) or retrain?(1): '))
+			
+			if opt == 0:
+				pass
 
-	#Pipeline for above steps
-	pipeline = Pipeline([
-		("vector",CountVectorizer(analyzer=tokenize)),
-		("tfidf",TfidfTransformer()),
-		("classifier", MultinomialNB())
-	])
+			elif opt == 1:
+				spam_detector_SVM = build_classifier(message_train, message_test, label_train, label_test)
 
-	scores = cross_val_score(pipeline,message_train,label_train,cv=10,scoring='accuracy',n_jobs=-1)
-	#print scores
+			else:
+				print 'Invalid option.'
+				sys.exit(0)
 
-	params = { 'tfidf__use_idf' : (True, False)}
-	tuned_clf_NB = GridSearchCV(pipeline,params, n_jobs=-1,scoring='accuracy', cv= StratifiedKFold(label_train,n_folds=5))
-	print len(label_train)
-	spam_detector = tuned_clf_NB.fit(message_train,label_train)
-
+		except:
+			## creates a new classifier
+			print 'Existing Classifier not found. Building Classifier from dataset.'
+			spam_detector_SVM = build_classifier(message_train, message_test, label_train, label_test)
+			
 	
-	test_arr = [ "FREE FREE FREE. Dial Now!", "miss you mom", "Consult now to win prizes", "Love Pizza, Win chances to eat unlimited pizza",
-		"this shit sucks main"]
+		test_arr = [ "FREE FREE FREE. Dial Now!", "miss you mom", "Consult now to win prizes", 
+				"Love Pizza, Win chances to eat unlimited pizza", "this shit sucks main"]
 
-	print spam_detector.predict(test_arr)
-	for i in test_arr:
-		print spam_detector.predict_proba([i])[0]
+		
+		
+		verbose_predict_sms(spam_detector_SVM,test_arr)
+		#print spam_detector_SVM.best_estimator_
+		
+		classifier_info(spam_detector_SVM, message_test, label_test)
 
-	# print spam_detector.best_estimator_
-	# print spam_detector.grid_scores_
-	# plot = plot_learning_curve(pipeline,"",message_train,label_train,cv=10)
-	# plt.show()
+		# Save the classifier in pickle file for loading instant spam_detector_classifier
+		with open("spam_detector_classifier.pkl","wb") as classifier:
+			cPickle.dump(spam_detector_SVM,classifier)
+
+	except Exception as e:
+		print e
 
 
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
-    """
-    Generate a simple plot of the test and traning learning curve.
+def predict_sms(estimator, array):
+	print '\nPredictions:\n=============\n'
+	predictions = estimator.predict(array)
+	for i in range(len(array)):
+		#print predictions[i] , "  :  ", array[i]
+		print '{:<6}'.format(predictions[i]), ": ", array[i]
+	print
 
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
+def verbose_predict_sms(estimator, array):
+	''' give detailed information about the prediction '''
+	print '\nPredictions:\n=============\n'
+	#print '    %s              %s     ' % ("HAM","SPAM")
+	predictions = estimator.predict(array)
+	for i in range(len(array)):
+		print
+		print '{:<6}'.format(predictions[i]), ": ", array[i]
+		print
+		print 'Probabilities (HAM / SPAM)'
+		print estimator.predict_proba(array[i])[0]
+		print ' -------------------------------- '
 
-    title : string
-        Title for the chart.
 
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
+def classifier_info(estimator, message_test, label_test):
+	''' defines information about the classifier '''
 
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
+	print '\nclassifier used:\n----------------\n'
+	print estimator.grid_scores_
 
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
+	print "Accuracy: ", accuracy_score(label_test, estimator.predict(message_test))
+	print "\nConfusion Matrix:\n-----------------\n"
+	print confusion_matrix(label_test, estimator.predict(message_test))
+	print "\nclassification report:\n----------------------"
+	print classification_report(label_test, estimator.predict(message_test))
 
-    cv : integer, cross-validation generator, optional
-        If an integer is passed, it is the number of folds (defaults to 3).
-        Specific cross-validation objects can be passed, see
-        sklearn.cross_validation module for the list of possible objects
 
-    n_jobs : integer, optional
-        Number of jobs to run in parallel (default 1).
-    """
+def build_classifier(message_train, message_test, label_train, label_test):
+
+	###################################
+	### Naive Bayes implementation ####
+	###################################
+
+	# print label_test
+	# #Pipeline_NB for above steps
+	# pipeline_NB = Pipeline([
+	# 	("vector",CountVectorizer(analyzer=tokenizer)),
+	# 	("tfidf",TfidfTransformer()),
+	# 	("classifier", MultinomialNB())
+	# ])
+
+	# scores = cross_val_score(pipeline_NB,message_train,label_train,cv=10,scoring='accuracy',n_jobs=-1)
+	# #print scores
+
+	# params_NB = { 'tfidf__use_idf' : (True, False)}
+	# tuned_clf_NB = GridSearchCV(pipeline_NB,params_NB, n_jobs=-1,scoring='accuracy', cv= StratifiedKFold(label_train,n_folds=5))
+	
+	# spam_detector_NB = tuned_clf_NB.fit(message_train,label_train)
+
+	# return spam_detector_NB
+
+
+	######################################
+	######################################
+	
+
+	############################
+	# ### SVM implementation ###
+	# ##########################
+
+	pipeline_SVM = Pipeline([
+			("vector", CountVectorizer(analyzer=tokenizer)),
+			("tfidf", TfidfTransformer()),
+			("classifier", SVC(probability=True	)),
+		]) 
+
+	params_SVM = [{ "classifier__C": [1,10,50,100], "classifier__kernel":["linear","rbf"], "classifier__gamma":[.001,.0001]}]
+
+	tuned_clf_SVM = GridSearchCV(pipeline_SVM, params_SVM, refit="True", n_jobs=-1,
+							scoring="accuracy",cv=StratifiedKFold(label_train,n_folds=5))
+
+	spam_detector_SVM = tuned_clf_SVM.fit(message_train,label_train)
+
+	return spam_detector_SVM
+	
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """  Generate a simple plot of the test and traning learning curve. """
+
     plt.figure()
     plt.title(title)
     if ylim is not None:
@@ -141,7 +213,7 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     return plt
 
 
-def tokenize(message):
+def tokenizer(message):
 	''' Tokenize/Lemmatize the words'''
 	message = unicode(message,'utf-8').lower()
 	message = remove_punctuation_unicode(message)
